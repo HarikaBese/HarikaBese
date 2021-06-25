2c5c20e70cfc372e0655eb03bf2e69e21e3f947f
HarikaBese <86481685+HarikaBese@users.noreply.github.com>
2021-06-25 21:01:42 +0530

    Add files via upload

---
+++ New folder (3)/Evenodd.java
@@ -0,0 +1,11 @@
+class Evenodd
+{
+public static void main(String args[])
+{
+int a=101;
+if((a&1)==1)
+System.out.println("Given number is odd no:");
+else
+System.out.println("Given number is even no:");
+}
+}
